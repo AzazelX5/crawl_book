782693b05a7c138356c6fed0ac80ccbb61830af8
赵乾颖 <Azazel-5052@live.cn>
2017-12-13 15:28:22 +0800

    Update manage.py
    [MODIFY]修改manage.py格式(#3)

--- src/manage.py
+++ src/manage.py
@@ -9,11 +9,11 @@ if __name__ == '__main__':
     if len(input_data) == 2 and 'rebuild_db' in input_data[1]:
         # 重建数据库
         rebuild_db()
-	elif len(input_data) == 2 and 'crawl' in input_data[1]:
-		# 爬取
-		crawl_book()
-	elif len(input_data) == 3 and 'crawl' in input_data[1]:
+    elif len(input_data) == 2 and 'crawl' in input_data[1]:
+	# 爬取
+	crawl_book()
+    elif len(input_data) == 3 and 'crawl' in input_data[1]:
         # 爬取指定页数
         crawl_book(input_data[2])
-	else:
-		print('参数输入有误！')
+    else:
+ 	print('参数输入有误！')
